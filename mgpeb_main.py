52100a50404dc7ecf73ea11ba23e7fe681a13da0
Traiba <matheustraiba@gmail.com>
2026-04-25 20:38:42 -0300

    feat: defining the modules

---
+++ mgpeb_main.py
@@ -0,0 +1,15 @@
+prioridades = {
+    "Extrema": 5,
+    "Alta": 4,
+    "Média-Alta": 3,
+    "Média": 2,
+    "Baixa": 1
+}
+
+modulos_missao = [
+    {"id": "MOD-01", "nome": "Suporte Médico", "massa": 14200, "criticidade": prioridades["Extrema"]},
+    {"id": "MOD-02", "nome": "Geração de Energia", "massa": 9800, "criticidade": prioridades["Alta"]},
+    {"id": "MOD-03", "nome": "Habitação Principal", "massa": 18500, "criticidade": prioridades["Média-Alta"]},
+    {"id": "MOD-04", "nome": "Laboratório Científico", "massa": 11300, "criticidade": prioridades["Média"]},
+    {"id": "MOD-05", "nome": "Logística e Suprimentos", "massa": 22700, "criticidade": prioridades["Baixa"]}
+]
